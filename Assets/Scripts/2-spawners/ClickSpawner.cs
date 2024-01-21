62b908a3d5fa995cdb283e1ef2bcf8c11aa1bb9a
Yonatan Tal <talyonatantal@gmail.com>
2024-01-21 14:20:42 +0200

    Week 3 - Part A

--- Assets/Scripts/2-spawners/ClickSpawner.cs
+++ Assets/Scripts/2-spawners/ClickSpawner.cs
@@ -1,44 +1,68 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/**
- * This component spawns the given object whenever the player clicks a given key.
- */
-public class ClickSpawner: MonoBehaviour {
+public class ClickSpawner : MonoBehaviour
+{
     [SerializeField] protected InputAction spawnAction = new InputAction(type: InputActionType.Button);
-    [SerializeField] protected GameObject prefabToSpawn;
+    [SerializeField] protected GameObject[] lasers; // Array of different laser prefabs
     [SerializeField] protected Vector3 velocityOfSpawnedObject;
+    [SerializeField] protected float spawnDelay; // Delay between spawns
 
-    void OnEnable()  {
+    private bool canSpawn = true;
+    private int shotCounter = 0;
+
+    void OnEnable()
+    {
         spawnAction.Enable();
     }
 
-    void OnDisable()  {
+    void OnDisable()
+    {
         spawnAction.Disable();
     }
 
-    protected virtual GameObject spawnObject() {
+    protected virtual GameObject spawnObject()
+    {
+        if (!canSpawn)
+        {
+            return null;
+        }
+
         Debug.Log("Spawning a new object");
 
+        // Determine which laser prefab to use based on shot count
+        int lasersIndex = 0;
+        if(shotCounter%5==0 && shotCounter!=0){lasersIndex=1;}
+
         // Step 1: spawn the new object.
-        Vector3 positionOfSpawnedObject = transform.position;  // span at the containing object position.
-        Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
-        GameObject newObject = Instantiate(prefabToSpawn, positionOfSpawnedObject, rotationOfSpawnedObject);
+        Vector3 positionOfSpawnedObject = transform.position;
+        Quaternion rotationOfSpawnedObject = Quaternion.identity;
+        GameObject newObject = Instantiate(lasers[lasersIndex], positionOfSpawnedObject, rotationOfSpawnedObject);
 
         // Step 2: modify the velocity of the new object.
         Mover newObjectMover = newObject.GetComponent<Mover>();
-        if (newObjectMover) {
+        if (newObjectMover)
+        {
             newObjectMover.SetVelocity(velocityOfSpawnedObject);
         }
 
+        StartCoroutine(SpawnCooldown());
+        shotCounter += 1;
         return newObject;
     }
-    
-    private void Update() {
-        if (spawnAction.WasPressedThisFrame()) {
+
+    IEnumerator SpawnCooldown()
+    {
+        canSpawn=false;
+        yield return new WaitForSeconds(spawnDelay);
+        canSpawn=true;
+    }
+
+    private void Update()
+    {
+        if (spawnAction.triggered)
+        {
             spawnObject();
         }
     }
